cbe6076423190879b65fb9312dcb505fc05b8eaf
beegle98 <ckwjdans@ajou.ac.kr>
2022-12-21 17:14:55 +0900

    1620 not solved

---
+++ 1620/1620.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    int n,m;
+    string x,ans[100001];
+    map<string,string> map_name;
+    map<string,string> map_index;
+    scanf("%d %d",&n,&m);
+    for(int i=1;i<=n;i++){
+        cin >> x;
+        //cout << to_string(i) <<"\n";
+        map_index.insert(make_pair(to_string(i),x));
+        map_name.insert(make_pair(x,to_string(i)));
+    }
+
+    for(int i=0;i<m;i++){
+        cin >> x;
+        
+        if('0'<=x[0] && x[0]<='9')
+        {
+            //printf("%s\n",map_index.at(x).c_str());
+            ans[i] = map_index.at(x);
+        }
+        else ans[i] = map_name.at(x);
+                
+    }
+    for(int i=0;i<m;i++){
+        cout << ans[i] << '\n';
+    }
+    return 0;
+}
