98b7a209062ace9141fe84d97dbf4b2243327c8a
beegle98 <ckwjdans@ajou.ac.kr>
2022-12-27 11:16:22 +0900

    11054 solved

--- 11054/11054.cpp
+++ 11054/11054.cpp
@@ -23,24 +23,26 @@ int main(){
 	for (int i = 0; i < n; i++){
 		cin >> s[i];
 		dp1[i] = 1;
-        dp2[i]=1;
 		for (int j = 0; j < i; j++){
 			if (s[i] > s[j] && dp1[i]<=dp1[j]){
 				dp1[i] = dp1[j] + 1;
             }
 		}
-        for(int j=i-1;j>=0;j--){
+        
+	}
+    for (int i=n-1;i>=0;i--){
+        dp2[i]=1;
+        for(int j=n-1;j>i;j--){
             if(s[i] > s[j] && dp2[i]<=dp2[j]){
                 dp2[i]=dp2[j]+1;
             }
         }
-	}
+    }
     for(int i=0;i<n;i++){
-        printf("%d %d\n",dp1[i],dp2[i]);
         if(max < dp1[i]+dp2[i]){
             max = dp1[i]+dp2[i];
         }
     }
-	cout << max;
+	cout << max-1;
 	return 0;
 }
