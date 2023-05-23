07bbc181951ae2468578957b6bccfc5ba9a2369a
beegle98 <ckwjdans@ajou.ac.kr>
2023-05-23 17:19:38 +0900

    K등분 solving

--- SCPC/k등분.cpp
+++ SCPC/k등분.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 typedef long long ll;
-
+const int N=500005;
 const ll MOD=1e9+7;
-ll dp[500001],v[500001],s[500001];
+ll dp[N],v[N],s[N];
+ll Fac[N],Inv[N];
 int n,k;
+
+ll comb(int n, int r){ return r > n ? 0 : Fac[n] * Inv[r] % MOD * Inv[n-r] % MOD; }
 
 int main(){
     ios::sync_with_stdio(false);
@@ -15,11 +18,14 @@ int main(){
 
     // aM 일때 dp[a]+=dp[a-1]
     // dp[k-1]이 정답
-    // M==0 이면 0 개수 (x)C(k-1)
+    
 
     int T, test_case;
     ll sum=0, m, x, Answer;
     cin >> T;
+    Fac[0]=1;
+    for(int i=1;i<N;i++){Fac[i]=Fac[i-1] * i % MOD;}
+    for(int i=0;i<N;i++){}
 	for(test_case = 0; test_case  < T; test_case++)
 	{
         cin >> n >> k;
@@ -37,10 +43,13 @@ int main(){
         m=s[n-1]/k;
         dp[0]=1;
         if(m==0){
-            // n C r
+            // M==0 이면 0 개수 (x)C(k-1)
             
             x=0;
-
+            for(int i=0;i<n;i++){
+                if(s[i]==0) x++;
+                Answer = comb(x,k-1);
+            }
         }
         else{
             for(int i=0;i<n;i++){
