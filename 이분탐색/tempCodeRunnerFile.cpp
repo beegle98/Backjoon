d301ad55fe0de3157de780e81f6e74fd451be657
beegle98 <ckwjdans@ajou.ac.kr>
2023-06-01 10:15:21 +0900

    2110(이분탐색) solved

---
+++ 이분탐색/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
