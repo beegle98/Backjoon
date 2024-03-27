1a0f8dd8a355ece76ef922a28dc89bdac720fab6
beegle98 <ckwjdans@ajou.ac.kr>
2024-03-27 23:14:57 +0900

    세그먼트 트리 기본 개념

--- test/test.cpp
+++ test/test.cpp
@@ -11,9 +11,13 @@ int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
     
-    string s;
-    cin >> s;
-    cout << s << "??!";
+    int n[10];
+    int sum=0;
+    for(int i=0;i<5;i++){
+        cin >> n[i];
+        sum+=n[i]*n[i];
+    }
+    cout << sum%10;
     return 0;
 
 }
