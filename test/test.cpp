8f705bb1bb4e6ae26b684d92a52cd4578248b803
beegle98 <ckwjdans@ajou.ac.kr>
2024-03-28 23:43:56 +0900

    test

--- test/test.cpp
+++ test/test.cpp
@@ -6,18 +6,22 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<int> vi;
 
+int p(string s){
+    int n = s.length();
+    for(int i=0;i<n/2;i++){
+        if(s[i]!=s[n-1-i]) return 0;
+    }
+    return 1;
+}
+
 int main(){
     ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
     
-    int n[10];
-    int sum=0;
-    for(int i=0;i<5;i++){
-        cin >> n[i];
-        sum+=n[i]*n[i];
-    }
-    cout << sum%10;
+    int n;
+    cin >> n;
+    cout << (n+8-1)%8;
     return 0;
 
 }
