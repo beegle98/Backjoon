646d6b50b769602d27f268640e576b35583581c9
beegle98 <ckwjdans@ajou.ac.kr>
2024-04-05 23:32:27 +0900

    test

--- test/test.cpp
+++ test/test.cpp
@@ -15,7 +15,15 @@ int main()
     
     int a,b,c;
 
-    cin >> a >> b;
-    cout << a%b;
+    cin >> a >> b >>c;
+    if(a==b && b==c) cout << 10000+a*1000;
+    else if(a==b) cout << 1000+a*100;
+    else if(b==c) cout << 1000+b*100;
+    else if(c==a) cout << 1000+c*100;
+    else{
+        int MAX;
+        MAX=max(max(a,b),c);
+        cout << 100*MAX;
+    }
     return 0;
 }
